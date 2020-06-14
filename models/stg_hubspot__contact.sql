dbc0341fd0b0f8d4d91626d36c5674be840eb11a
Dylan Baker <dylang.baker@googlemail.com>
2020-06-14 16:58:49 -0400

    sources and one mdoels

---
+++ models/stg_hubspot__contact.sql
@@ -0,0 +1,18 @@
+{%- set columns = adapter.get_columns_in_relation(source('hubspot','contact')) -%}
+
+with base as (
+
+    select *
+    from {{ source('hubspot','contact') }}
+
+), fields as (
+
+    select
+        id as contact_id,
+        {{ remove_prefix_from_columns(columns=columns, exclude=['id']) }}
+    from base
+
+)
+
+select *
+from fields
