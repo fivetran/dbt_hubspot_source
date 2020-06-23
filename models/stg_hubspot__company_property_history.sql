973d2d759f38f152800897efd4263a15ae4dbbf9
Dylan Baker <dylang.baker@googlemail.com>
2020-06-23 13:07:32 -0400

    history

---
+++ models/stg_hubspot__company_property_history.sql
@@ -0,0 +1,21 @@
+with base as (
+
+    select *
+    from {{ source('hubspot','company_property_history')}}
+
+), fields as (
+
+    select
+        _fivetran_synced,
+        company_id,
+        name as field_name,
+        source as change_source,
+        source_id as change_source_id,
+        timestamp as change_timestamp,
+        value as new_value
+    from base
+    
+)
+
+select *
+from fields
