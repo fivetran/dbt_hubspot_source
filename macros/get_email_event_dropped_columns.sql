d0638f88c8dc28efe661aab683f3081d4f59eae1
Dylan Baker <dylan@dbanalytics.co>
2020-11-17 08:45:42 -0500

    update from email again

--- macros/get_email_event_dropped_columns.sql
+++ macros/get_email_event_dropped_columns.sql
@@ -6,11 +6,16 @@
     {"name": "cc", "datatype": dbt_utils.type_string()},
     {"name": "drop_message", "datatype": dbt_utils.type_string()},
     {"name": "drop_reason", "datatype": dbt_utils.type_string()},
-    {"name": "from", "datatype": dbt_utils.type_string()},
     {"name": "id", "datatype": dbt_utils.type_string()},
     {"name": "reply_to", "datatype": dbt_utils.type_string()},
     {"name": "subject", "datatype": dbt_utils.type_string()}
 ] %}
+
+{% if target.type == 'snowflake' %}
+ {{ columns.append({"name": "FROM", "datatype": dbt_utils.type_string(), "quote": True, "alias": "from_email"}) }}
+{% else %}
+ {{ columns.append({"name": "from", "datatype": dbt_utils.type_string(), "quote": True, "alias": "from_email"}) }}
+{% endif %}
 
 {{ return(columns) }}
 
