c61a9cc08f93fa58c24e436a4657fac7884a292f
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2021-07-26 14:56:45 -0700

    doing stuff

--- models/stg_hubspot__contact.sql
+++ models/stg_hubspot__contact.sql
@@ -20,21 +20,34 @@ with base as (
 ), fields as (
 
     select
-        _fivetran_deleted,
-        _fivetran_synced,
-        property_email as email,
         id as contact_id,
+
+{% if var('hubspot__pass_through_all_columns', false) %}
+        -- just pass everything through
+        {{ 
+            fivetran_utils.remove_prefix_from_columns(
+                columns=adapter.get_columns_in_relation(ref('stg_hubspot__contact_tmp')), 
+                prefix='property_', exclude=['id', 'property_contact_id']) 
+        }}
+    from base
+
+{% else %}
+        -- just default columns + explicitly configured passthrough columns
+        property_email as email,
         property_company as contact_company,
         property_firstname as first_name,
         property_lastname as last_name,
         property_createdate as created_at,
         property_jobtitle as job_title,
-        property_annualrevenue as company_annual_revenue
+        property_annualrevenue as company_annual_revenue,
+        _fivetran_deleted,
+        _fivetran_synced
 
         --The below macro adds the fields defined within your hubspot__contact_pass_through_columns variable into the staging model
         {{ fivetran_utils.fill_pass_through_columns('hubspot__contact_pass_through_columns') }}
 
     from macro
+{% endif %}    
     
 )
 
