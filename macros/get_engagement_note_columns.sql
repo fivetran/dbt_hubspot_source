f25dd36444a0a4c0380c88c6b2c695636ceb47cc
Joe Markiewicz <74217849+fivetran-joemarkiewicz@users.noreply.github.com>
2023-07-13 13:00:22 -0500

    review updates

--- macros/get_engagement_note_columns.sql
+++ macros/get_engagement_note_columns.sql
@@ -4,6 +4,7 @@
     {"name": "_fivetran_synced", "datatype": dbt.type_timestamp()},
     {"name": "_fivetran_deleted", "datatype": dbt.type_boolean()},
     {"name": "body", "datatype": dbt.type_string(), "alias": "note"},
+    {"name": "type", "datatype": dbt.type_string(), "alias": "engagement_type"},
     {"name": "engagement_id", "datatype": dbt.type_int()},
     {"name": "property_hs_createdate", "datatype": dbt.type_timestamp(), "alias": "created_timestamp"},
     {"name": "property_hs_timestamp", "datatype": dbt.type_timestamp(), "alias": "occurred_timestamp"},
--- macros/get_engagement_task_columns.sql
+++ macros/get_engagement_task_columns.sql
@@ -4,6 +4,7 @@
     {"name": "_fivetran_synced", "datatype": dbt.type_timestamp()},
     {"name": "_fivetran_deleted", "datatype": dbt.type_boolean()},
     {"name": "engagement_id", "datatype": dbt.type_int()},
+    {"name": "type", "datatype": dbt.type_string(), "alias": "engagement_type"},
     {"name": "property_hs_createdate", "datatype": dbt.type_timestamp(), "alias": "created_timestamp"},
     {"name": "property_hs_timestamp", "datatype": dbt.type_timestamp(), "alias": "occurred_timestamp"},
     {"name": "property_hubspot_owner_id", "datatype": dbt.type_int(), "alias": "owner_id"},
