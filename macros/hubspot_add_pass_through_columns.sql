fb9ee194364b387520740ee4bb8851c319602e80
Renee Li <renee.li@fivetran.com>
2024-10-08 17:51:09 -0400

    adjust add pass through macro

--- macros/hubspot_add_pass_through_columns.sql
+++ macros/hubspot_add_pass_through_columns.sql
@@ -1,23 +1,14 @@
 {% macro hubspot_add_pass_through_columns(base_columns, pass_through_var) %}
 
   {% if pass_through_var %}
-
     {% for column in pass_through_var %}
-
-      {% if column is mapping %}
-
-        {% if column.alias %}
-          {% do base_columns.append({ "name": adapter.quote(column.name), "alias": column.alias, "datatype": column.datatype if column.datatype else dbt.type_string()}) %}
-        {% else %}
-          {% do base_columns.append({ "name": adapter.quote(column.name), "datatype": column.datatype if column.datatype else dbt.type_string()}) %}
-        {% endif %}
-
+      {% set quoted_column_name = adapter.quote(column.name) %}
+      {% if column.alias %}
+        {% do base_columns.append({ "name": quoted_column_name, "alias": adapter.quote(column.alias) }) %}
       {% else %}
-        {% do base_columns.append({ "name": adapter.quote(column), "datatype": dbt.type_string()}) %}
+        {% do base_columns.append({ "name": quoted_column_name }) %}
       {% endif %}
-
     {% endfor %}
-
   {% endif %}
 
 {% endmacro %}
