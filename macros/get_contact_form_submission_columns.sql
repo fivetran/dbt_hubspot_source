971c223c21e4895fbde34f4af5a175c169aa1280
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2025-04-04 13:04:20 -0500

    update datatypes

--- macros/get_contact_form_submission_columns.sql
+++ macros/get_contact_form_submission_columns.sql
@@ -3,10 +3,10 @@
 {% set columns = [
     {"name": "_fivetran_synced", "datatype": dbt.type_timestamp()},
     {"name": "timestamp", "datatype": dbt.type_timestamp()},
-    {"name": "form_id", "datatype": dbt.type_string()},
-    {"name": "contact_id", "datatype": dbt.type_string()},
-    {"name": "conversion_id", "datatype": dbt.type_string()},
-    {"name": "page_id", "datatype": dbt.type_string()},
+    {"name": "form_id", "datatype": dbt.type_bigint()},
+    {"name": "contact_id", "datatype": dbt.type_bigint()},
+    {"name": "conversion_id", "datatype": dbt.type_bigint()},
+    {"name": "page_id", "datatype": dbt.type_bigint()},
     {"name": "page_url", "datatype": dbt.type_string()},
     {"name": "portal_id", "datatype": dbt.type_int()},
     {"name": "title", "datatype": dbt.type_string()}
