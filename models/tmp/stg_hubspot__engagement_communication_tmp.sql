7ece4a2ae5b9352faa78214f9cb3d675cb48736f
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2025-04-14 12:03:39 -0500

    update vars

--- models/tmp/stg_hubspot__engagement_communication_tmp.sql
+++ models/tmp/stg_hubspot__engagement_communication_tmp.sql
@@ -1,4 +1,4 @@
-{{ config(enabled=fivetran_utils.enabled_vars(['hubspot_sales_enabled','hubspot_engagement_enabled','hubspot_engagement_communication_enabled'])) }}
+{{ config(enabled=fivetran_utils.enabled_vars(['hubspot_sales_enabled','hubspot_engagement_enabled']) and var('hubspot_engagement_communication_enabled', false)) }}
 
 select {{ dbt_utils.star(source('hubspot','engagement_communication')) }}
 from {{ var('engagement_communication') }}
