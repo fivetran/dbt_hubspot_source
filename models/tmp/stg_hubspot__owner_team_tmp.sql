9f8893a605d10954c3c46cb36f84d5e1cf23a407
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2025-04-15 09:42:45 -0500

    Update models/tmp/stg_hubspot__owner_team_tmp.sql

--- models/tmp/stg_hubspot__owner_team_tmp.sql
+++ models/tmp/stg_hubspot__owner_team_tmp.sql
@@ -1,4 +1,4 @@
-{{ config(enabled=fivetran_utils.enabled_vars(['hubspot_team_enabled', 'hubspot_owner_team_enabled'])) }}
+{{ config(enabled=fivetran_utils.enabled_vars(['hubspot_team_enabled'])) }}
 
 select {{ dbt_utils.star(source('hubspot','owner_team')) }}
 from {{ var('owner_team') }}
