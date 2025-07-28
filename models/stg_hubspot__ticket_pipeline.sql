e58f66ba7bced5d7a4e6e29b9a7cc6cd1d009c23
Avinash Kunnath <avinash.kunnath@fivetran.com>
2025-07-28 13:28:55 -0700

    PR suggestions

--- models/stg_hubspot__ticket_pipeline.sql
+++ models/stg_hubspot__ticket_pipeline.sql
@@ -23,7 +23,7 @@ fields as (
 final as (
 
     select
-        pipeline_id as ticket_pipeline_id,
+        cast(pipeline_id as {{ dbt.type_string() }}) as ticket_pipeline_id,
         _fivetran_deleted as is_ticket_pipeline_deleted,
         cast(_fivetran_synced as {{ dbt.type_timestamp() }}) as _fivetran_synced,
         active as is_active,
